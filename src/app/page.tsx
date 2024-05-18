db49b2edfa4459051e8133705f97bd6e180c2afc
10inoino <inomatahi@icloud.com>
2024-05-19 06:26:32 +0900

    install react-markdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
 import Image from "next/image";
+import Markdown from "react-markdown";
 
 export default function Home() {
+  const markdown = '# Hi, *Pluto*!'
   return (
-    <h1>matty resume</h1>
+    <>
+      <h1>matty resume</h1>
+      <Markdown>{markdown}</Markdown>
+    </>
   );
 }
