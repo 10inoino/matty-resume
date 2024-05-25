e9a0d5a00b3f65a71be065cf46515a7ddd893a1d
10inoino <inomatahi@icloud.com>
2024-05-26 07:19:50 +0900

    prettier

--- src/components/self-introduce.tsx
+++ src/components/self-introduce.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from "react";
 
 const SelfIntroduce = () => {
-    return (
-        <div>
-            <h2>自己紹介</h2>
-            <ul>
-                <li>名前：猪股 光(イノマタ ヒカル)</li>
-                <li>生年月日：1996/09/06</li>
-                <li>ニックネーム：matty(マティと読みます)</li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h2>自己紹介</h2>
+      <ul>
+        <li>名前：猪股 光(イノマタ ヒカル)</li>
+        <li>生年月日：1996/09/06</li>
+        <li>ニックネーム：matty(マティと読みます)</li>
+      </ul>
+    </div>
+  );
 };
 
 export default SelfIntroduce;
