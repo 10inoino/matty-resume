c74a87d78c942238a8b37a6e2a3b8ecfb04640f3
Hikaru Inomata <57826628+10inoino@users.noreply.github.com>
2024-05-27 09:01:40 +0900

    Add content (#6)
    * create contents
    
    * prettier
    
    * fix
    
    * fix

---
+++ src/components/histories.tsx
@@ -0,0 +1,126 @@
+const Histories = () => {
+  return (
+    <div>
+      <h2>Histories</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>年月</th>
+            <th>内容</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1996年</td>
+            <td>東京都で生まれる</td>
+          </tr>
+          <tr>
+            <td>1999年</td>
+            <td>親の方針で、神奈川の堂山町に引っ越す（この頃の記憶は無い）</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td>中学校までスキップ</td>
+          </tr>
+          <tr>
+            <td>2009年4月</td>
+            <td>堂山町立堂山中学校入学</td>
+          </tr>
+          <tr>
+            <td>2012年3月</td>
+            <td>堂山町立堂山中学校卒業</td>
+          </tr>
+          <tr>
+            <td>2012年4月</td>
+            <td>神奈川県立立堂高校入学</td>
+          </tr>
+          <tr>
+            <td>2015年3月</td>
+            <td>神奈川県立立堂高校卒業</td>
+          </tr>
+          <tr>
+            <td>2015年4月</td>
+            <td>明治大学 理工学部 情報工学科 入学</td>
+          </tr>
+          <tr>
+            <td>2018年</td>
+            <td>
+              内定が決まったときから
+              <a
+                href="https://www.persol-pt.co.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                パーソルプロセス&テクノロジー株式会社
+              </a>
+              の新卒採用でアルバイト
+            </td>
+          </tr>
+          <tr>
+            <td>2019年3月</td>
+            <td>明治大学 理工学部 情報工学科 卒業</td>
+          </tr>
+          <tr>
+            <td>2019年4月</td>
+            <td>
+              <a
+                href="https://www.persol-pt.co.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                パーソルプロセス&テクノロジー株式会社
+              </a>{" "}
+              入社
+            </td>
+          </tr>
+          <tr>
+            <td>2020年4月</td>
+            <td>
+              <a
+                href="https://www.code4japan.org/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                一般社団法人コード・フォー・ジャパン
+              </a>
+              にプロボノとしてジョイン
+            </td>
+          </tr>
+          <tr>
+            <td>2020年7月</td>
+            <td>千葉県船橋市で一人暮らしを始める</td>
+          </tr>
+          <tr>
+            <td>2021年4月</td>
+            <td>
+              部署の人化に伴い、
+              <a
+                href="https://www.postas.co.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ポスタス株式会社
+              </a>
+              に転籍
+            </td>
+          </tr>
+          <tr>
+            <td>2022年6月</td>
+            <td>
+              <a
+                href="https://graffer.jp/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                株式会社グラファー
+              </a>
+              に転職。Product Developerとして活動。
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Histories;
